b035a20eec7bc657c48a0e1081b145ac0077bb49
AiWeb <weiaiyanling@yeah.net>
2023-03-18 18:51:28 +0800

    Update dap.lua

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -20,28 +20,88 @@ dap.configurations.python = {
 }
 
 ---------C,C++ --------------
--- dap.adapters.lldb = {
--- 	type = "executable",
--- 	command = "/usr/local/opt/llvm/bin/lldb-vscode", -- adjust as needed, must be absolute path
--- 	name = "lldb",
+dap.adapters.lldb = {
+	type = "executable",
+	command = "/usr/local/opt/llvm/bin/lldb-vscode", -- adjust as needed, must be absolute path
+	name = "lldb",
+}
+dap.configurations.cpp = {
+	{
+		name = "Launch",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = true,
+		args = {},
+		env = function()
+			local variables = {}
+			for k, v in pairs(vim.fn.environ()) do
+				table.insert(variables, string.format("%s=%s", k, v))
+			end
+			return variables
+		end,
+	},
+}
+
+-- If you want to use this for Rust and C, add something like this:
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+
+-- dap.adapters.codelldb = {
+--   type = 'server',
+--   port = "${port}",
+--   executable = {
+--     -- CHANGE THIS to your path!
+--     command = os.getenv("HOME") .. '/.local/share/nvim/mason/bin/codelldb',
+--     args = {"--port", "${port}"},
+--
+--     -- On windows you may have to uncomment this:
+--     -- detached = false,
+--   }
 -- }
 -- dap.configurations.cpp = {
--- 	{
--- 		name = "Launch",
--- 		type = "lldb",
--- 		request = "launch",
--- 		program = function()
--- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
--- 		end,
--- 		cwd = "${workspaceFolder}",
--- 		stopOnEntry = true,
--- 		args = {},
--- 	},
+--   {
+--     name = "Launch file",
+--     type = "codelldb",
+--     request = "launch",
+--     program = function()
+--       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--     end,
+--     cwd = '${workspaceFolder}',
+--     stopOnEntry = true,
+--   },
 -- }
---
--- -- If you want to use this for Rust and C, add something like this:
 -- dap.configurations.c = dap.configurations.cpp
 -- dap.configurations.rust = dap.configurations.cpp
+
+------------------- javascript/node---------------------
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require'dap.utils'.pick_process,
+  },
+}
 
 local dap_breakpoint_color = {
 	breakpoint = {
